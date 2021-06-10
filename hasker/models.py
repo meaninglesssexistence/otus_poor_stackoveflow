762f3cc8f7ab4bd60df4d4d46e872969a30d592b
null <askill@pm.me>
2021-06-10 19:06:11 +0300

    Добавил свойство user_avatar в модель UserAvatar.

--- hasker/models.py
+++ hasker/models.py
@@ -3,6 +3,7 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from django.templatetags.static import static
 from django.utils import timezone
 
 
@@ -10,6 +11,12 @@ class UserAvatar(models.Model):
     avatar = models.ImageField(blank=True, upload_to='avatars')
     user = models.OneToOneField(
         settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+    @property
+    def avatar_url(self):
+        if self.avatar:
+            return self.avatar.url
+        return static('img/no-avatar.png')
 
     def __str__(self):
         return self.user.username
